3e44d126353562c3d4564e9a02f0888411428a6e
Jesusfc <jfcaraballo@gmail.com>
2024-04-16 16:39:48 +0200

    update microservice architecture - Eureka service and more

---
+++ src/main/java/com/jesusfc/demo/controller/Service2Controller.java
@@ -0,0 +1,25 @@
+package com.jesusfc.demo.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Author Jesús Fdez. Caraballo
+ * Created on abr - 2024
+ */
+@AllArgsConstructor
+@RequestMapping("/service_2")
+@RestController
+public class Service2Controller {
+
+
+    @GetMapping("/get")
+    public ResponseEntity<String> getService2() {
+        return new ResponseEntity<>("El servicio 2 responde OK", HttpStatus.OK);
+    }
+
+}
