da1bf36f6a119fef2a106b329a8e86124ab29e88
jfcaraballo <jfcaraballo@gmail.com>
2024-04-17 14:33:13 +0200

    update eureka client for services 1 and 2

---
+++ src/main/java/com/jesusfc/demo/config/LocalDiscovery.java
@@ -0,0 +1,14 @@
+package com.jesusfc.demo.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Author Jesús Fdez. Caraballo
+ * jesus.fdez.caraballo@gmail.com
+ * Created on abr - 2024
+ */
+@EnableDiscoveryClient
+@Configuration
+public class LocalDiscovery {
+}
